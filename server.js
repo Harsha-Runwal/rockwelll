6df9a69c9d69c36626a4d561152465bc89627074
Harsha Runwal <106861144+Harsha-Runwal@users.noreply.github.com>
2024-12-04 15:33:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,13 +18,13 @@ app.get('/hello', function (req, res) {
   res.send(person);
 });
 
-//  app.listen(8081, '0.0.0.0', () => {
-//     console.log('Example app listening on http://localhost:8081');
-// });
-var server = app.listen(8081, function () {
-  var host = server.address().address
-  var port = server.address().port
-  console.log("host = ",host)
- console.log("port = ",port)
-  console.log("Example app is listening at http://localhost:8081", host, port)
-})
+ app.listen(9459, '15.207.113.73', () => {
+    console.log('Example app is listening on http://15.207.113.73:9459');
+});
+// var server = app.listen(9459, function () {
+//   var host = server.address().address
+//   var port = server.address().port
+//   console.log("host = ",host)
+//  console.log("port = ",port)
+//   console.log("Example app is listening at http://localhost:9459", host, port)
+// })
