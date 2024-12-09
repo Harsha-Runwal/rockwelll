5b750f7f1d00a1022208d8b6dfd84696203c96cd
Harsha Runwal <106861144+Harsha-Runwal@users.noreply.github.com>
2024-12-09 11:20:58 +0530

    Updated server.js v2

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/hello', function (req, res) {
 });
 
  app.listen(9459, '0.0.0.0', () => {
-    console.log('Example V-1.0 app is listening on http://0.0.0.0:9459');
+    console.log('Example V-2.0 app is listening on http://0.0.0.0:9459');
 });
 // var server = app.listen(9459, function () {
 //   var host = server.address().address
