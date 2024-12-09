5528a4cf903baeef392b741b2dd7e15c63159900
Harsha Runwal <106861144+Harsha-Runwal@users.noreply.github.com>
2024-12-09 11:07:13 +0530

    Updated server.js with V1

--- server.js
+++ server.js
@@ -3,14 +3,12 @@ var app = express();
 var path=require("path");
 var fs=require("fs");
 
-
 //server configuration
 app.use(express.static(path.join(__dirname,'public')));
 
  app.get('/', function (req, res) {
    res.sendFile(path.join(___dirname + '/index.html'));
 });
- 
 
 app.get('/hello', function (req, res) {
   console.log("CAlling rest api");
@@ -19,7 +17,7 @@ app.get('/hello', function (req, res) {
 });
 
  app.listen(9459, '0.0.0.0', () => {
-    console.log('Example app is listening on http://0.0.0.0:9459');
+    console.log('Example V-1.0 app is listening on http://0.0.0.0:9459');
 });
 // var server = app.listen(9459, function () {
 //   var host = server.address().address
